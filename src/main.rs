c94527bd483d2f7804af8e5471b6b082b42a151c
Julia Naomi <jnboeira@outlook.com>
2020-07-09 00:00:41 -0300

    use j4rs errors

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-use j4rs::{JvmBuilder,InvocationArg, MavenArtifact};
+use j4rs::{JvmBuilder,InvocationArg, MavenArtifact, errors::J4RsError};
 use std::convert::TryFrom;
 
 static PEER: &str = "datomic.Peer";
 static URI: &str = "datomic:dev://localhost:4334/hello";
 
-fn main() {
-    let jvm = JvmBuilder::new().build().unwrap();
-    jvm.deploy_artifact(&MavenArtifact::from("com.datomic:datomic-pro:1.0.6165")).unwrap();
+fn main() -> Result<(), J4RsError>{
+    let jvm = JvmBuilder::new().build()?;
+    jvm.deploy_artifact(&MavenArtifact::from("com.datomic:datomic-pro:1.0.6165"))?;
 
-    let uri = InvocationArg::try_from(URI).unwrap();
-
-    // jvm.static_class(PEER).unwrap();
-    // jvm.invoke_static(
-    //     PEER,
-    //     "createDatabase",
-    //     &[uri]
-    // ).unwrap();
-
+    let uri = InvocationArg::try_from(URI)?;
 
     let peer = jvm.create_instance(
         PEER,
         &Vec::new(),
-    ).unwrap();
-
-    let json_instance = jvm.invoke(
-        &peer,
+    )?;
+    
+    jvm.invoke_static(
+        PEER,
         "createDatabase",
-        &[InvocationArg::try_from(URI).unwrap()], 
-    ).unwrap();
+        &[uri]
+    )?;
+
+    Ok(())
 }
